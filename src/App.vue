bc43068335cb98a278480f0d798ae2091bf4b8aa
iamtheblackunicorn <officialrealalexanderabraham@gmail.com>
2022-06-19 17:02:12 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <br/>
+ <br/>
+ <br/>
+  <br/>
+ <br/>
+ <br/>
+ <ColorMatch name="Color Match" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ColorMatch from './components/ColorMatch.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ColorMatch
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ColorMatch.vue
@@ -0,0 +1,32 @@
+<template>
+ <h1>{{ name }}</h1>
+ <input v-model="start"/>
+ <button @click="shiftColor()">SHIFT</button>
+ <p>Complementary: {{ result }}</p>
+</template>
+
+<script>
+export default {
+
+  name: 'HeadingText',
+  props: ['name'],
+
+  data() {
+    return {
+       start: '',
+       result: ''
+    };
+  },
+  methods: {
+    shiftColor(){
+      var tinycolor = require("tinycolor2");
+      console.log(this.start);
+      this.result = tinycolor(this.start).complement().toHexString();
+    }
+  }  
+}
+</script>
+
+<style lang="scss">
+@import '../sass/main.scss';
+</style>
